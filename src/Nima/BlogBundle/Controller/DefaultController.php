17c4905bda6cd38cf4d9f96e14d30f03a0155e83
Vladimir Nimchyn <nima.vova@gmail.com>
2016-11-25 15:34:35 +0200

    generate bundle

---
+++ src/Nima/BlogBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nima\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('NimaBlogBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Nima/BlogBundle/NimaBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nima\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NimaBlogBundle extends Bundle
+{
+}
